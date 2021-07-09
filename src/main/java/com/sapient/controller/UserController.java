1db08e57834d7bc6c1b93fbeb41d78987e6cb071
aritra <aritramustafi56@gmail.com>
2021-07-09 13:01:08 +0530

    asdfghm

--- src/main/java/com/sapient/controller/UserController.java
+++ src/main/java/com/sapient/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.sapient.controller;
 
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,7 +38,7 @@ public class UserController {
 	}
 	
 	@PostMapping(value="/api/insertuser")
-	public User insertUser(User user) {
+	public User insertUser(@RequestBody User user) {
 		return iud.save(user);
 	}
 	
--- src/main/java/com/sapient/entity/User.java
+++ src/main/java/com/sapient/entity/User.java
@@ -6,56 +6,101 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="user")
+@Table(name = "user")
 public class User {
-	
+
 	@Id
 	private int user_id;
+
+	@Column
+	private String name;
 	@Column
 	private String email;
 	@Column
 	private String password;
 	@Column
-	private String user_role;
-	
-	
+	private String role;
+
+
 	
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public User(int user_id, String email, String password, String user_role) {
+
+
+
+	public User(int user_id, String name, String email, String password, String role) {
 		super();
 		this.user_id = user_id;
+		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.user_role = user_role;
+		this.role = role;
 	}
+
+
+
 	public int getUser_id() {
 		return user_id;
 	}
+
+
+
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
+
 	public String getPassword() {
 		return password;
 	}
+
+
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getUser_role() {
-		return user_role;
+
+
+
+	public String getRole() {
+		return role;
 	}
-	public void setUser_role(String user_role) {
-		this.user_role = user_role;
+
+
+
+	public void setRole(String role) {
+		this.role = role;
 	}
-	
-	
-	
+
+
+
+
+		
 }
